7d34a3561cd01fd449f467bb6317d73bff664e7f
Yaroslav <slavik92@meta.ua>
2021-05-29 14:19:55 +0300

    add files

---
+++ app.js
@@ -0,0 +1,30 @@
+//ДЗ_16_Завдання №3
+let n = +prompt ("Введіть день тижня!");
+    
+    switch (n) {
+        case 1:
+          console.log('Понеділок!');
+          break;
+        case 2:
+          console.log('Вівторок!');
+              break;
+        case 3:
+          console.log('Середа!');
+              break;
+        case 4:
+          console.log('Четвер!');
+          break;
+        case 5:
+          console.log('Пятниця!');
+          break;
+        case 6:
+          console.log('Субота!');
+          break;
+        case 7:
+          console.log('Неділя!');
+          break;
+         
+        default:
+          console.log(null);
+      }
+
